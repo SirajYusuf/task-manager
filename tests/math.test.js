d6167fa263db212ba2939e2bb1f8e847c0be95d5
siraj-yusuf <yusuf@skystoptechnologies.com>
2022-01-31 19:43:18 +0530

    first express test

---
+++ tests/math.test.js
@@ -0,0 +1,35 @@
+const {calculateTip,celsiusToFahrenheit,fahrenheitToCelsius,add} = require('../src/math')
+
+test('should calculate total with tip',()=>{
+    const total = calculateTip(10,.3)
+    expect(total).toBe(13)
+})
+
+test('Should convert 32 F to 0 C',()=>{
+    const total =fahrenheitToCelsius(86)
+    expect(total).toBe(30)
+})
+
+test('Should convert 0 C to 32 F',()=>{
+    const total =  celsiusToFahrenheit(30)
+    expect(total).toBe(86)
+})
+
+// test('Async test demo',(done)=>{
+//     setTimeout(()=>{
+//        expect(1).toBe(2)
+//        done()
+//     },2000)
+// })
+
+test('should add two numbers',(done)=>{
+    add(2,3).then((sum)=>{
+        expect(sum).toBe(5)
+        done()
+    })
+})
+
+test('add two numbers async/await',async()=>{
+    const sum = await add(10,22)
+    expect(sum).toBe(32)
+})
---
+++ tests/user.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest')
+const app = require('../src/app')
+
+test('should signup a new user',async()=>{
+    await request(app).post('/addUser').send({
+        name:'Siraj',
+        email:'sirajygs@gmail.com',
+        password:'sirajygs'
+    }).expect(201)
+})
